69492647a96a2c5c05deec8a4feaa71a77f69e01
mayowadavid <mayowadavid100@gmail.com>
2021-05-24 11:52:59 +0100

    solve problem

--- urlConfig.js
+++ urlConfig.js
@@ -1,4 +1,4 @@
-const baseUrl= location.hostName === "localhost" ? "http://localhost:4000" : "https://backend-logistics.herokuapp.com/"
+const baseUrl=  "https://backend-logistics.herokuapp.com/"
 export const api = `${baseUrl}/api`;
 export const generatePublicUrl = (fileName) => {
     return `${baseUrl}/public/${fileName}`
