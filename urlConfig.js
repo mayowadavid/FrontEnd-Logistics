894df4211bd10c4410e9904a150166d68a65a05c
mayowadavid <mayowadavid100@gmail.com>
2021-05-24 14:24:25 +0100

    url change

--- urlConfig.js
+++ urlConfig.js
@@ -1,5 +1,5 @@
 const baseUrl=  "https://backend-logistics.herokuapp.com"
-export const api = `${baseUrl}api`;
+export const api = `${baseUrl}/api`;
 export const generatePublicUrl = (fileName) => {
     return `${baseUrl}/public/${fileName}`
 }
