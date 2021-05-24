bc1072cedb3305d4541918089146745140cb3f99
mayowadavid <mayowadavid100@gmail.com>
2021-05-24 13:32:40 +0100

    update api url

--- urlConfig.js
+++ urlConfig.js
@@ -1,4 +1,4 @@
-const baseUrl=  "https://backend-logistics.herokuapp.com/"
+const baseUrl=  "https://backend-logistics.herokuapp.com"
 export const api = `${baseUrl}/api`;
 export const generatePublicUrl = (fileName) => {
     return `${baseUrl}/public/${fileName}`
