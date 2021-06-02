152e1d3a79099098d9f4fa8aefdafcdd3f145916
mayowadavid <mayowadavid100@gmail.com>
2021-06-02 03:04:25 +0100

    initial

--- components/firebase.js
+++ components/firebase.js
@@ -14,7 +14,7 @@ const firebaseConfig = {
 
   const firebaseApp = !firebase.apps.length ? firebase.initializeApp(firebaseConfig) :firebase.app();
   const store = firebaseApp.storage();
-  const database = firebase.firestore();
+  const database = firebaseApp.firestore();
 
 
   export {store, database};
